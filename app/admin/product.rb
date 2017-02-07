4554b23248a9604db8f2f0a78d4fe52fa2dcf8ae
Mansur <mansurmabo@gmail.com>
2017-02-07 16:47:06 +0600

    add activeadmin views and forms

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,17 +1,43 @@
 ActiveAdmin.register Product do
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  permit_params :title, :description, :price, :image, :category_id
 
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :description
+    column :price
+    column :image
+    column :category_id do |article|
+      article.category.title if article.category.present?
+    end
+
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :title
+      row :description
+      row :price
+      row :image
+      row :category_id
+    end
+  end
+  form do |f|
+    inputs 'Details' do
+      f.input :title
+      f.input :description
+      f.input :price
+      f.input :image, as: :file
+      f.input :category_id, as: :select, collection: Category.all, prompt: 'Выберите категорию товара'
+    end
+
+    f.actions
+  end
+
+  filter :title
+  filter :created_at
 
 end
