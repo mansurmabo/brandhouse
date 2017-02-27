c2d850ea7df3533433b7061feb717610f6b50b9c
Farid <farid.babazov@gmail.com>
2017-02-27 23:13:11 +0600

    add 'new' action to product controller, create form for add product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,24 @@
 class ProductsController < ApplicationController
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(products_params)
+    if @product.save
+      redirect_to @product
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def products_params
+    params.require(:product).permit(:vendor_code, :title, :brand, :availability ,:price ,:description ,:image)
+  end
+
 
 end
