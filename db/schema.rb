45fb2b00515f9aa6095ae84aad99bb5be517fd22
Farid <farid.babazov@gmail.com>
2017-02-28 15:37:46 +0600

    remove column 'image' from Product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227162939) do
+ActiveRecord::Schema.define(version: 20170228093641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 20170227162939) do
     t.string   "title"
     t.text     "description"
     t.decimal  "price"
-    t.string   "image"
     t.integer  "category_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
