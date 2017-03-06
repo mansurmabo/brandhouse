f1118946c3d51561949b2878f064d1112bc263ff
Ramazan <ramablack93@gmail.com>
2017-03-06 13:02:32 +0600

    added routes for carts

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  resources :carts, only: [:index] do
+    get 'add_to_cart', on: :collection
+    get 'destroy_cart', on: :collection
+    delete 'remove_product', on: :collection
+    get 'increase_quantity', on: :collection
+    get 'reduce_quantity', on: :collection
+  end
+
 end
+
+
