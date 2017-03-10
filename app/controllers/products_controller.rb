8edfb018a7d8df1a9a064ba39ac1524581802496
Ramazan <ramablack93@gmail.com>
2017-03-10 17:43:35 +0600

    added paginate params to products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < BaseController
   # skip_before_action check_current_user only: [:index, :show]
 
   def index
-    @products = Product.all.order(id: :desc)
+    @products = Product.paginate(page: params[:page], per_page: 1).order(id: :desc)
   end
 
   def new
