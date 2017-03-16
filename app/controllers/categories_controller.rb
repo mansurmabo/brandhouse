edf7c65d5d1751fcf4bffc9a5f8153f9ff0d9079
Ramazan <ramablack93@gmail.com>
2017-03-16 14:25:35 +0600

    added pagination for filtered products from categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @products = @category.products
+    @products = @category.products.paginate(page: params[:page], per_page: 1).order(created_at: :desc)
     @categories = Category.all
     @brands = Brand.all
   end
