06a419a1d66a7d5b8ebc9829e02cebdc9776637e
Ramazan <ramablack93@gmail.com>
2017-03-16 13:12:51 +0600

    commented activeadmin

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,27 +1,27 @@
-ActiveAdmin.register Category do
-
-  permit_params :title, :ancestry
-  index do
-    selectable_column
-    id_column
-    column :title
-    actions
-  end
-  show do
-    attributes_table do
-      row :title
-    end
-  end
-  form do |f|
-    inputs 'Details' do
-      f.input :title
-      f.input :ancestry, :as => :select, :collection => Category.all.map {|u| [u.title, u.id]}
-    end
-
-    f.actions
-  end
-
-  filter :title
-  filter :created_at
-
-end
+# ActiveAdmin.register Category do
+#
+#   permit_params :title, :ancestry
+#   index do
+#     selectable_column
+#     id_column
+#     column :title
+#     actions
+#   end
+#   show do
+#     attributes_table do
+#       row :title
+#     end
+#   end
+#   form do |f|
+#     inputs 'Details' do
+#       f.input :title
+#       f.input :ancestry, :as => :select, :collection => Category.all.map {|u| [u.title, u.id]}
+#     end
+#
+#     f.actions
+#   end
+#
+#   filter :title
+#   filter :created_at
+#
+# end
