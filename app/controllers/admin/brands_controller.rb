433ad4b6d47d30c57a132de54ab53d1af8db4561
Ramazan <ramablack93@gmail.com>
2017-03-29 12:12:27 +0600

    added CRUD for admin/brands_controller

--- app/controllers/admin/brands_controller.rb
+++ app/controllers/admin/brands_controller.rb
@@ -1,4 +1,44 @@
 class Admin::BrandsController < Admin::AdminController
   
+  def index
+    @brands = Brand.all
+  end
 
+  def new
+    @brand = Brand.new
+  end
+
+  def create
+    @brand = Brand.new(brands_params)
+    if @brand.save
+      redirect_to admin_brands_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @brand = Brand.find(params[:id])
+  end
+
+  def update
+    @brand = Brand.find(params[:id])
+    if @brand.update(brands_params)
+      redirect_to admin_brands_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @brand = Brand.find(params[:id])
+    @brand.destroy
+    redirect_to :back
+  end
+
+  private
+
+  def brands_params
+    params.require(:brand).permit(:title)
+  end
 end
